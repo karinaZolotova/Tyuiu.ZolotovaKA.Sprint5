38d9dee8739b06690d0817a154558527c890190d
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-08 00:13:57 +0500

    Task2

---
+++ Tyuiu.ZolotovaKA.Sprint5.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ZolotovaKA.Sprint5.Task2.V12.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint5.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            //C:\Users\Ïîëüçîâàòåëü\AppData\Local\Temp\OutPutFileTask2.txt
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+            FileInfo fl = new FileInfo(path);
+            bool fle = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fle);
+        }
+    }
+}
---
+++ Tyuiu.ZolotovaKA.Sprint5.Task2.V12/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ZolotovaKA.Sprint5.Task2.V12.Lib;
+static void Main(string[] args)
+{
+    DataService ds = new DataService();
+    int[,] matrix = new int[3, 3] { { -5, -5, 9 }, { -7, 3, -4 }, { 8, 7, 9 } };
+    int rows = matrix.GetUpperBound(0) + 1;
+    int colums = matrix.Length / rows;
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+    string res = ds.SaveToFileTextData(matrix);
+    Console.WriteLine("файл создан!" + res);
+    Console.ReadKey();
+}
+}
