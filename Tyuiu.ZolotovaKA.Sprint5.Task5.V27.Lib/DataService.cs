3334eccbf4cbf71fe7bdd62f31782571ea0dbd99
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-08 00:25:23 +0500

    Task5

---
+++ Tyuiu.ZolotovaKA.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZolotovaKA.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double r = 0; int i = 0;
+            //       using StreamReader sr = new StreamReader(path);
+            //     string line;
+            //   while ((line = sr.ReadLine()) != null)
+            // {
+            //    if (Convert.ToDouble(line.Replace('.',','))%5==0)
+            //   {
+            //      r += Convert.ToDouble(line.Replace('.', ','));
+            //     i++;
+            //}
+            //}
+            // return r/i;
+
+
+            string[] a = File.ReadAllText(path).Split(' ');
+
+            foreach (string s in a)
+            {
+                if (Convert.ToDouble(s.Replace('.', ',').Trim(' ')) % 5 == 0)
+                {
+                    r += Convert.ToDouble(s.Replace('.', ',').Trim(' ').Trim().Trim('\n')); i++;
+                }
+            }
+            return r / i;
+        }
+    }
+}
---
+++ Tyuiu.ZolotovaKA.Sprint5.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.ZolotovaKA.Sprint5.Task5.V27.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint5.Task5.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            Assert.AreEqual(7.5, ds.LoadFromDataFile(Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V27.txt")));
+        }
+    }
+}
